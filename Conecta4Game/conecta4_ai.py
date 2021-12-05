f0ad91908ed47af3187092a4a9eb6f4fd4bc80ec
lucasrebra <lucasrebra97@gmail.com>
2021-12-05 17:52:04 +0100

    Corregido PEP-8 excepto problema de longitud en determinadas lineas en connecta4_ai: falta corregir PEP-8 en el archivo de funciones y en el main

--- Conecta4Game/conecta4_ai.py
+++ Conecta4Game/conecta4_ai.py
@@ -1,61 +1,62 @@
-import pygame, random
+import pygame
+import random
 from Settings import *
 from conecta4Functions import *
 
+
 # Funciones de la IA del juego
-def funcion_puntua(ventana_deslizante,Pieza):
-    puntuacion=0
-    pieza_contrario= PLAYER_PIECE
+def funcion_puntua(ventana_deslizante, Pieza):
+    puntuacion = 0
+    pieza_contrario = PLAYER_PIECE
     if Pieza == PLAYER_PIECE:
-        pieza_contrario= AI_PIECE
-    
+        pieza_contrario = AI_PIECE
     if ventana_deslizante.count(Pieza) == 4:
             puntuacion += 100
     elif ventana_deslizante.count(Pieza) == 3 and ventana_deslizante.count(VACIO) == 1:
             puntuacion += 5
     elif ventana_deslizante.count(Pieza) == 2 and ventana_deslizante.count(VACIO) == 2:
             puntuacion += 2
-
-    if ventana_deslizante.count(pieza_contrario)==3 and ventana_deslizante.count(VACIO)==1:
+    if ventana_deslizante.count(pieza_contrario) == 3 and ventana_deslizante.count(VACIO) == 1:
             puntuacion -= 4
 
     return puntuacion
 
-def puntuacion_heuristica(Tablero, Pieza):
-    puntuacion=0
 
-    ##Score center column
+def puntuacion_heuristica(Tablero, Pieza):
+    puntuacion = 0
+
+    # Score center column
     vector_centro = [int(i) for i in list(Tablero[:, NColumnas//2])]
     cuenta_centro = vector_centro.count(Pieza)
     puntuacion += cuenta_centro * 3
 
-    ##Puntuar horizontalmente
+    # Puntuar horizontalmente
     for F in range(NFilas):
-        vector_filas=[int(t) for t in list(Tablero[F, :])]
+        vector_filas = [int(t) for t in list(Tablero[F, :])]
         for C in range(NColumnas-3):
-            ventana_deslizante = vector_filas[C:C+ANCHO_VENTANA] 
-            puntuacion += funcion_puntua(ventana_deslizante,Pieza)
+            ventana_deslizante = vector_filas[C:C+ANCHO_VENTANA]
+            puntuacion += funcion_puntua(ventana_deslizante, Pieza)
 
-    ##Puntuar verticalmente
+    # Puntuar verticalmente
     for C in range(NColumnas):
-        vector_columnas=[int(i) for i in list(Tablero[:,C])]
+        vector_columnas = [int(i) for i in list(Tablero[:, C])]
         for F in range(NFilas-3):
             ventana_deslizante = vector_columnas[F:F+ANCHO_VENTANA]
-            puntuacion += funcion_puntua(ventana_deslizante,Pieza)
+            puntuacion += funcion_puntua(ventana_deslizante, Pieza)
 
-    ##Puntuar diagonalmente positivo
-
+    # Puntuar diagonalmente positivo
     for F in range(NFilas-3):
-        for C in range (NColumnas-3):
-            ventana_deslizante = [Tablero[F+i][C+i] for i in range (ANCHO_VENTANA)]
-            puntuacion += funcion_puntua(ventana_deslizante,Pieza)
-    ##Puntuar diagonalmente negativo
+        for C in range(NColumnas-3):
+            ventana_deslizante = [Tablero[F+i][C+i] for i in range(ANCHO_VENTANA)]
+            puntuacion += funcion_puntua(ventana_deslizante, Pieza)
 
+    # Puntuar diagonalmente negativo
     for F in range(NFilas-3):
-        for C in range (NColumnas-3):
+        for C in range(NColumnas-3):
             ventana_deslizante = [Tablero[F+3-i][C+i] for i in range(ANCHO_VENTANA)]
-            puntuacion += funcion_puntua(ventana_deslizante,Pieza)
+            puntuacion += funcion_puntua(ventana_deslizante, Pieza)
     return puntuacion
+
 
 def es_nodo_final(Tablero):
     return winning_move(Tablero, PLAYER_PIECE) or winning_move(Tablero, AI_PIECE) or len(pos_validas(Tablero)) == 0
@@ -64,10 +65,11 @@ def es_nodo_final(Tablero):
 def pos_validas(Tablero):
     posiciones_v = []
     for col in range(NColumnas):
-        if movidaLegal(Tablero,col):
+        if movidaLegal(Tablero, col):
             posiciones_v.append(col)
 
     return posiciones_v
+
 
 def minimax(Tablero, profundidad, alpha, beta, maximizingPlayer):
     localizaciones_validas = pos_validas(Tablero)
@@ -75,23 +77,23 @@ def minimax(Tablero, profundidad, alpha, beta, maximizingPlayer):
     if profundidad == 0 or nodo_final:
         if nodo_final:
             if winning_move(Tablero, AI_PIECE):
-                return (None, 1000000000000000000) #inf
+                return (None, 1000000000000000000)  # inf
             elif winning_move(Tablero, PLAYER_PIECE):
-                return (None,-10000000000000000000) #menos inf
-            else: #No + mov validos
+                return (None, -10000000000000000000)  # menos inf
+            else:  # No + mov validos
                 return (None, 0)
-        else: #Profundidad 0
+        else:  # Profundidad 0
             return (None, puntuacion_heuristica(Tablero, AI_PIECE))
-    
+
     if maximizingPlayer:
         valor = -math.inf
         columna = random.choice(localizaciones_validas)
         for col in localizaciones_validas:
             row = filaDisp(Tablero, col)
             copia_tablero = Tablero.copy()
-            print("MAX: ",row, " - ", col)
-            soltarPieza(copia_tablero, col, row, AI_PIECE) # Cambio de columna y row, no entiendo
-            nueva_puntuacion = minimax(copia_tablero,profundidad-1, alpha, beta, False)[1]
+            print("MAX: ", row, " - ", col)
+            soltarPieza(copia_tablero, col, row, AI_PIECE)  # Cambio de columna y row, no entiendo
+            nueva_puntuacion = minimax(copia_tablero, profundidad-1, alpha, beta, False)[1]
             if nueva_puntuacion > valor:
                 valor = nueva_puntuacion
                 columna = col
@@ -106,9 +108,9 @@ def minimax(Tablero, profundidad, alpha, beta, maximizingPlayer):
         for col in localizaciones_validas:
             row = filaDisp(Tablero, col)
             copia_tablero = Tablero.copy()
-            print("MIN: ",row, " - ", col)
-            soltarPieza(copia_tablero, col, row, PLAYER_PIECE) # Cambio de column y row, no entiendo
-            nueva_puntuacion =  minimax(copia_tablero,profundidad-1, alpha, beta, True)[1]
+            print("MIN: ", row, " - ", col)
+            soltarPieza(copia_tablero, col, row, PLAYER_PIECE)  # Cambio de column y row, no entiendo
+            nueva_puntuacion = minimax(copia_tablero, profundidad-1, alpha, beta, True)[1]
             if nueva_puntuacion < valor:
                 valor = nueva_puntuacion
                 columna = col
@@ -116,28 +118,28 @@ def minimax(Tablero, profundidad, alpha, beta, maximizingPlayer):
             if alpha >= beta:
                 break
         return columna, valor
-    
+
 
 def agente(Tablero, Pieza):
-     posiciones_v=pos_validas(Tablero)
-     mejor_puntuacion=100000
-     mejor_col=random.choice(posiciones_v)
-     for y in posiciones_v:
-         x=filaDisp(Tablero,y)
-         Tablero_auxiliar= Tablero.copy()
-         soltarPieza(Tablero_auxiliar,y,x,Pieza)
-         puntuacion=puntuacion_heuristica(Tablero_auxiliar,Pieza)
-         if puntuacion> mejor_puntuacion:
-             mejor_puntuacion = puntuacion
-             mejor_col= y
+    posiciones_v = pos_validas(Tablero)
+    mejor_puntuacion = 100000
+    mejor_col = random.choice(posiciones_v)
+    for y in posiciones_v:
+        x = filaDisp(Tablero, y)
+        Tablero_auxiliar = Tablero.copy()
+        soltarPieza(Tablero_auxiliar, y, x, Pieza)
+        puntuacion = puntuacion_heuristica(Tablero_auxiliar, Pieza)
+        if puntuacion > mejor_puntuacion:
+            mejor_puntuacion = puntuacion
+            mejor_col = y
 
-     return mejor_col
+    return mejor_col
 
 
 def juega_AI(tablero, ventana, font, FIN):
     Player(draw_text, ventana, AI_PIECE)
-   
-    #x = agente(tablero,AI_PIECE)
+
+    # x = agente(tablero,AI_PIECE)
     x, minimax_score = minimax(tablero, 5, -math.inf, math.inf, True)
 
     if movidaLegal(tablero, x):
@@ -148,7 +150,7 @@ def juega_AI(tablero, ventana, font, FIN):
             pygame.draw.rect(ventana, NEGRO, (0, 0, NColumnas*TAMFI, TAMFI))
             SPL = str(AI_PIECE)
             S = 'player '+SPL+' WIIIINS!!'
-            TXT = font.render(S,1 , BLANCO)
+            TXT = font.render(S, 1, BLANCO)
             ventana.blit(TXT, (10, 10))
             FIN = True
 
