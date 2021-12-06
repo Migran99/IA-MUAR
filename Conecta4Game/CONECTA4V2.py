b31b2ce3cb9855f07f32e35112308cc60e35391c
lucasrebra <lucasrebra97@gmail.com>
2021-12-06 13:41:34 +0100

    Corregido PEP8 en programa main, queda comentarios funciones conecta4funciones

--- Conecta4Game/CONECTA4V2.py
+++ Conecta4Game/CONECTA4V2.py
@@ -2,14 +2,15 @@ import numpy as np
 from numpy.lib.stride_tricks import broadcast_arrays
 from scipy.signal import convolve2d
 from Settings import *
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 import math
 from VICTORY import *
 from conecta4_ai import *
 from conecta4Functions import *
 
-#inicializacion de las variables y de programas
+# Inicializacion de las variables y de programas
 pygame.init()
 
 tablero = crearTablero()
@@ -21,26 +22,27 @@ Turno = 0
 
 pygame.display.update()
 
-##################Programa principal###########################
+# ----------------- Programa principal --------------------- #
 
 while not FIN:
     if STATE == 'start':
         Start_draw(draw_text, ventana)
-        STATE =  Start_events(STATE)
+        STATE = Start_events(STATE)
     elif STATE == 'playing':
         DIB_TABLERO(tablero, ventana)
         for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
-                    sys.exit() 
+                    sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    #Jugador 1
+                    # Jugador 1
                     if Turno == 0:
-                        FIN = player_turn(1, tablero, ventana, event, FONT, FIN)
+                        FIN = player_turn(1, tablero, ventana,
+                                          event, FONT, FIN)
                         print("Jugando jugador 1")
-                    #Jugador 2 - AI
+                    # Jugador 2 - AI
                     else:
-                        #TUPL(2)
+                        # TUPL(2)
                         FIN = juega_AI(tablero, ventana, FONT, FIN)
 
                     DIB_TABLERO(tablero, ventana)
@@ -50,4 +52,4 @@ while not FIN:
 
                     if FIN:
                         pygame.time.wait(3500)
-                        #VIDEO()
+                        # VIDEO()
